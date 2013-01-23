c1430ecd23dfe189598d3a9c27e2429c5252c21f
jimmieha <jimmiehallman@130-229-9-197-dhcp.wlan.ki.se>
2013-01-23 12:14:21 +0100

    writing in setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+name='biostuff',
+version='0.1dev',
+author='John Doe',
+author_email='johndoe@example.com',
+url='example.com',
+packages=['biostuff',],
+license='GPLv3',
+long_description=open('README.txt').read(),
+)
