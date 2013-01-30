f60f2c9a93bfd2ff3e6c125356dd2dd9f91c1f14
jimmieha <jimmiehallman@villemakelamacbookpro.scilifelab.se>
2013-01-30 09:49:41 +0100

    Adding assignment 2

---
+++ scripts/getting_data.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from hallman import getting_data
+	
+getting_data()
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
+import sys, os
 
 setup(
-name='biostuff',
-version='0.1dev',
-author='John Doe',
-author_email='johndoe@example.com',
-url='example.com',
-packages=['biostuff',],
-license='GPLv3',
-long_description=open('README.txt').read(),
-)
+	name='hallman',
+	#version='0.2',
+	author='Jimmie Hallman',
+	author_email='johndoe@example.com',
+	url='example.com',
+	license='GPLv3',
+	#packages = find_packages(),
+	scripts = ['scripts/getting_data.py'],
+	#long_description=open('README.txt').read(),
+	)
